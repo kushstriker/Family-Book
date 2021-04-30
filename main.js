8debb019324b16ebc5dffc7d4daef7c626e0e3e8
kushstriker <83347729+kushstriker@users.noreply.github.com>
2021-04-30 17:17:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var friends = [];
+var img = ["https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX10522309.jpg","https://image.freepik.com/free-vector/cartoon-school-boy-carrying-backpack-waving-hand_29190-4926.jpg","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.clipartkey.com%2Fview%2FThJmbJ_6-year-old-boy-cartoon%2F&psig=AOvVaw3XSgIHFotBQ4YsZkKywZ4_&ust=1619600195344000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjH6ImHnvACFQAAAAAdAAAAABAE"];
 
-function submit(){
+var reason = ["This is my mother Mamatha",
+              "This is me Kushal",
+              "This is my brother Yashas"];
 
-var friend1 = document.getElementById("name_of_the_student_1").value;
-var friend2 = document.getElementById("name_of_the_student_2").value;
-friends.push(friend1);
-friends.push(friend2);
-document.getElementById("display_name").innerHTML= friends;
-document.getElementById("submit_button").style.display="none";
+var i = 0;
+function nextslide(){
+  document.getElementById("album").src=img[i];
+  document.getElementById("name").innerHTML=reason[i];
+  i++;
+  if( i>3 ) i=0;
+  
 }
+ 
+
+  
+
+ 
